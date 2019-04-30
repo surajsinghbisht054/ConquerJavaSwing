8b50296f20d06c8fa092169ffa12e30cda80cea3
surajsinghbisht054 <surajsinghbisht054@gmail.com>
2019-04-30 12:47:30 +0530

    new update by group member

---
+++ LEVEL2/ReflectMsg.java
@@ -0,0 +1,45 @@
+/*
+	Author:
+		Suraj Singh Bisht
+		surajsinghbisht054@gmail.com
+
+*/
+
+// import module
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+// Our GUI Class 
+class ReflectMsg {
+	private JFrame frame;
+	private JLabel label1;
+	private JLabel label2;
+	private JPanel panel1; 
+	
+
+	// function to prepare
+	private void prepareGUI(){
+
+	}
+
+	// constructor
+	ReflectMsg(){
+		prepareGUI();
+
+	}
+
+
+	// our main function
+	public static void main(String[] args){
+		ReflectMsg root = new ReflectMsg();
+
+	}
+
+
+}
+
+
+
